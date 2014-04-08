398ea7af2378d0b59fb8cae3388cca5fd63ae55d
Julio Biason <julioandre@cwi.com.br>
2014-04-08 13:05:30 -0300

    setup and install

--- luncho/__init__.py
+++ luncho/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-
+VERSION = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+from luncho import VERSION
+
+setup(name='luncho',
+      version=VERSION,
+      description='Democratic lunching',
+      long_description=file('README.md').read(),
+      author='Julio Biason',
+      author_email='julio.biason@gmail.com',
+      url='https://github.com/jbiason/lunch-o',
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Web Environment',
+          'Framework :: Flask',
+          'Intended Audience :: End Users/Desktop',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Internet :: WWW/HTTP :: WSGI',
+          'Topic :: Utilities',
+          'Operating System :: OS Independent'
+      ])
